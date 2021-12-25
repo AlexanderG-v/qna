636d938987e4bc7dd87d95df98c9f37a219ef9f5
AlexanderG-v <gorelikov.alexander@gmail.com>
2021-12-25 23:47:11 +0300

    Question controller with rspec

--- spec/factories/questions.rb
+++ spec/factories/questions.rb
@@ -1,6 +1,11 @@
 FactoryBot.define do
   factory :question do
-    title { "MyString" }
+    sequence(:title) { |n| "My#{n}String" }
+
     body { "MyText" }
+
+    trait :invalid do
+      title { nil }
+    end
   end
 end
