2e6c54d72d6d05193b14f369d5c3e6bf29acb7b0
AlexanderG-v <gorelikov.alexander@gmail.com>
2022-01-22 21:37:10 +0300

    User can add a link to answer and polimorfic

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -25,7 +25,11 @@ RSpec.describe QuestionsController, type: :controller do
       expect(assigns(:question)).to eq question
     end
 
-    it 'assigns new answer for question' do
+    it 'assigns new link for question' do
+      expect(assigns(:answer).links.first).to be_a_new(Link)
+    end
+
+    it 'assigns new answer for answer' do
       expect(assigns(:answer)).to be_a_new(Answer)
     end
 
