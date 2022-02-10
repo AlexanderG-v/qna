9e381f1a082e1c285fe35d23cf263bb7995e8e2f
AlexanderG-v <gorelikov.alexander@gmail.com>
2022-02-10 22:33:57 +0300

    Transferring the 'find_for_oauth' method to a separate service

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -17,66 +17,12 @@ RSpec.describe User, type: :model do
   describe '.find_for_oauth' do
     let!(:user) { create(:user) }
     let(:auth) { OmniAuth::AuthHash.new(provider: 'facebook', uid: '123456') }
+    let(:service) { double('FindForOauthService') }
     
-    context 'user already has authorization' do
-      it 'returns the user' do
-        user.authorizations.create(provider: 'facebook', uid: '123456')
-        expect(described_class.find_for_oauth(auth)).to eq user
-      end
-    end
-
-    context 'user has not authorization' do
-      context 'user already exists' do
-        let(:auth) { OmniAuth::AuthHash.new(provider: 'facebook', uid: '123456', info: { email: user.email }) }
-
-        it 'does not create new user' do
-          expect { described_class.find_for_oauth(auth) }.to_not change(described_class, :count)
-        end
-
-        it 'creates authorization for user' do
-          expect { described_class.find_for_oauth(auth) }.to change(user.authorizations, :count).by(1)
-        end
-
-        it 'creates authirization with provider and uid' do
-          authorization = described_class.find_for_oauth(auth).authorizations.first
-
-          expect(authorization.provider).to eq auth.provider
-          expect(authorization.uid).to eq auth.uid
-        end
-
-        it 'returns the user' do
-          expect(described_class.find_for_oauth(auth)).to eq user
-        end
-      end
-
-      context 'user does not exist' do
-        let(:auth) { OmniAuth::AuthHash.new(provider: 'facebook', uid: '123456', info: { email: 'new@user.com' }) }
-
-        it 'creates new user' do
-          expect { described_class.find_for_oauth(auth) }.to change(described_class, :count).by(1)
-        end
-
-        it 'returns new user' do
-          expect(described_class.find_for_oauth(auth)).to be_a(described_class)
-        end
-
-        it 'fills new email' do
-          user = described_class.find_for_oauth(auth)
-          expect(user.email).to eq auth.info[:email]
-        end
-
-        it 'creates authorization for user' do
-          user = described_class.find_for_oauth(auth)
-          expect(user.authorizations).to_not be_empty
-        end
-
-        it 'creates authorization with provider and uid' do
-          authorization = described_class.find_for_oauth(auth).authorizations.first
-
-          expect(authorization.provider).to eq auth.provider
-          expect(authorization.uid).to eq auth.uid
-        end
-      end
+    it 'call FindForOauthService' do
+      expect(FindForOauthService).to receive(:new).with(auth).and_return(service)
+      expect(service).to receive(:call)
+      described_class.find_for_oauth(auth)
     end
   end
 
