c81490a87a69a2975b7357eda40267c9e08bc4ab
AlexanderG-v <gorelikov.alexander@gmail.com>
2022-02-11 18:32:38 +0300

    feature spec authentication via vkontakte

--- spec/features/sign_up_spec.rb
+++ spec/features/sign_up_spec.rb
@@ -51,5 +51,38 @@ feature 'User can register in the system', "
         expect(page).to have_content 'Could not authenticate you from GitHub because "Invalid credentials"'
       end
     end
+
+    describe 'Vkontakte' do
+      it 'user sign in with correct data without email' do
+        visit new_user_registration_path
+        expect(page).to have_content 'Sign in with Vkontakte'
+
+        mock_auth_hash('vkontakte', email: nil)
+        click_link 'Sign in with Vkontakte'
+
+        fill_in 'Enter email', with: 'user@test.com'
+        click_on 'Send confirmation to email'
+        
+        open_email 'user@test.com'
+        current_email.click_link 'Confirm my account'
+        click_link 'Sign in with Vkontakte'
+
+        within '.navbar' do
+          expect(page).to have_content 'user@test.com'
+        end
+        expect(page).to have_content 'Ask question' 
+        expect(page).to have_content 'Successfully authenticated from Vkontakte account.'
+      end
+
+      it 'can handle authentication error' do
+        invalid_mock 'vkontakte'
+        visit new_user_registration_path
+        expect(page).to have_content 'Sign in with Vkontakte'
+
+        click_link 'Sign in with Vkontakte'
+
+        expect(page).to have_content 'Could not authenticate you from Vkontakte because "Invalid credentials"'
+      end
+    end
   end
 end
