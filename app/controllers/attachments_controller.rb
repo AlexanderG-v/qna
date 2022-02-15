746f1324897241550137021b0f68ff14419884f0
AlexanderG-v <gorelikov.alexander@gmail.com>
2022-02-15 20:17:45 +0300

    Refact attachments controller

--- app/controllers/attachments_controller.rb
+++ app/controllers/attachments_controller.rb
@@ -1,11 +1,9 @@
 class AttachmentsController < ApplicationController
   before_action :authenticate_user!
 
-  authorize_resource
-
   def destroy
     @attachment = ActiveStorage::Attachment.find(params[:id])
-    return unless current_user.author?(@attachment.record)
+    authorize! :destroy, @attachment
 
     @attachment.purge
     flash.now[:notice] = 'File was successfully deleted'
