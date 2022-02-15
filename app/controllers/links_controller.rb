3a760056f6981f3f475cf869f570983c1a3e7bbb
AlexanderG-v <gorelikov.alexander@gmail.com>
2022-02-15 20:26:19 +0300

    Refact links controller

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,11 +1,9 @@
 class LinksController < ApplicationController
   before_action :authenticate_user!
 
-  authorize_resource
-
   def destroy
     @link = Link.find(params[:id])
-    return unless current_user&.author?(@link.linkable)
+    authorize! :destroy, @link.linkable
 
     @link.destroy
     flash.now[:notice] = 'Link was successfully deleted'
