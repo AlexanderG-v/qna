b3c64662a3b9c7073d42410b882abacf37422d34
AlexanderG-v <gorelikov.alexander@gmail.com>
2022-02-18 18:22:49 +0300

    Implemented API for list of all users with Spec

--- spec/api/v1/users_spec.rb
+++ spec/api/v1/users_spec.rb
@@ -25,13 +25,59 @@ describe 'Users API', type: :request do
 
       it 'returns all public fields' do
         %w[id email role created_at updated_at].each do |attr|
-          expect(json[attr]).to eq me.send(attr).as_json
+          expect(json['user'][attr]).to eq me.send(attr).as_json
         end
       end
 
       it 'does not return private fields' do
         %w[password encrypted_password].each do |attr|
           expect(json).to_not have_key(attr)
+        end
+      end
+    end
+  end
+
+  describe 'GET /api/v1/users' do
+    let(:api_path) { '/api/v1/users' }
+
+    it_behaves_like 'API Authorizable' do
+      let(:method) { :get }
+    end
+
+    context 'authorized' do
+      let(:access_token) { create(:access_token) }
+      let(:me) { create(:user) }
+      let!(:users) { create_list(:user, 3) }
+      let(:user) { users.first }
+      let(:users_response) { json['users'].first }
+
+      before { get api_path, params: { access_token: access_token.token }, headers: headers }
+
+      it 'returns list of users except me' do
+        expect(json['users'].size).to eq users.size
+      end
+
+      it 'not returns me in list of users' do
+        json['users'].each do |user|
+          expect(user['id']).to_not eq me.id
+        end
+      end
+
+      it 'returns 200 status' do
+        expect(response).to be_successful
+      end
+
+      it 'returns all public fields' do
+        %w[id email role created_at updated_at].each do |attr|
+          expect(users_response[attr]).to eq user.send(attr).as_json
+        end
+      end
+
+      it 'does not return private fields' do
+        %w[password encrypted_password].each do |attr|
+          json['users'].each do |user|
+            expect(user).to_not have_key(attr)
+          end
         end
       end
     end
