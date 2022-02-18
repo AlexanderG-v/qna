3d577eed54dbe9296782664be442cec91ecfe71a
AlexanderG-v <gorelikov.alexander@gmail.com>
2022-02-19 00:00:59 +0300

    API for creating a question

--- app/controllers/api/v1/questions_controller.rb
+++ app/controllers/api/v1/questions_controller.rb
@@ -10,9 +10,24 @@ class Api::V1::QuestionsController < Api::V1::BaseController
     render json: @question
   end
 
+  def create
+    @question = current_resource_owner.questions.new(question_params)
+
+    if @question.save
+      render json: @question
+    else
+      render json: { errors: @question.errors }, status: :unprocessable_entity
+    end
+  end
+
   private
 
   def set_question
     @question = Question.with_attached_files.find(params[:id])
+  end
+
+  def question_params
+    params.require(:question).permit(:title, :body, links_attributes: %i[name url id],
+                                                    reward_attributes: %i[title image])
   end
 end
--- spec/api/v1/questions_spec.rb
+++ spec/api/v1/questions_spec.rb
@@ -1,11 +1,8 @@
 require 'rails_helper'
 
 describe 'Questions API', type: :request do
-  let(:headers) do 
-    { 'CONTENT_TYPE' => 'application/json',
-      'ACCEPT' => "application/json" }
-  end
-
+  let(:headers) { { "ACCEPT" => "application/json" } }
+  
   describe 'GET /api/v1/questions' do
     let(:api_path) { '/api/v1/questions' }
     
@@ -108,6 +105,70 @@ describe 'Questions API', type: :request do
         expect(json['question']['files'].first['url']).to eq Rails.application.routes.url_helpers.rails_blob_path(
           question.files.first, only_path: true
         )
+      end
+    end
+  end
+
+  describe 'POST /api/v1/questions' do
+    let(:method) { :post }
+    let(:user) { create :user }
+    let(:access_token) { create(:access_token, resource_owner_id: user.id) }
+    let(:api_path) { '/api/v1/questions' }
+    
+    it_behaves_like 'API Unauthorizable'
+
+    context 'authorized' do
+      context 'with valid attributes' do
+        let(:question) { attributes_for(:question, author: user) }
+        let(:question_response) { json['question'] }
+
+        before do
+          post api_path,
+               params: { access_token: access_token.token, question: question },
+               headers: headers
+        end
+
+        it 'creates a new Question' do
+          expect do 
+            post api_path, params: { access_token: access_token.token, question: question },
+                           headers: headers
+          end.to change(Question, :count).by(1)
+        end
+
+        it_behaves_like 'API Authorizable'
+    
+        it 'contains user object' do
+          expect(question_response['author']['id']).to eq access_token.resource_owner_id
+        end
+  
+        it 'creates a question with the correct attributes' do
+          expect(Question.last).to have_attributes question
+        end
+      end
+    
+      context 'with invalid attributes' do
+        let(:question) { attributes_for(:question, :invalid) }
+    
+        before do
+          post api_path,
+               params: { access_token: access_token.token, question: question },
+               headers: headers
+        end
+
+        it "doesn't save question, renders errors" do
+          expect do 
+            post api_path, params: { access_token: access_token.token, question: question },
+                           headers: headers
+          end.to_not change(Question, :count)
+        end
+
+        it 'returns status 422' do
+          expect(response.status).to eq 422
+        end
+
+        it 'returns error' do
+          expect(json['errors']).to_not be_nil
+        end
       end
     end
   end
