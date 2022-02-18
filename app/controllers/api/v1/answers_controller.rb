ac5cf3a49b2e52850304a51b7342515be60b6aea
AlexanderG-v <gorelikov.alexander@gmail.com>
2022-02-18 23:07:46 +0300

    Implemented API for a separate answer with Spec

--- app/controllers/api/v1/answers_controller.rb
+++ app/controllers/api/v1/answers_controller.rb
@@ -1,12 +1,21 @@
 class Api::V1::AnswersController < Api::V1::BaseController
-  before_action :set_question, only: %i[index]
+  before_action :set_answer, only: %i[show]
+  before_action :set_question, only: %i[index show]
 
   def index
     @answers = @question.answers
     render json: @answers
   end
 
+  def show
+    render json: @answer
+  end
+
   private
+
+  def set_answer
+    @answer = Answer.with_attached_files.find(params[:id])
+  end
 
   def set_question
     @question ||= params[:question_id] ? Question.with_attached_files.find(params[:question_id]) : @answer.question
--- spec/api/v1/answers_spec.rb
+++ spec/api/v1/answers_spec.rb
@@ -38,4 +38,58 @@ describe 'Answers API', type: :request do
       end
     end
   end
+
+  describe 'GET /api/v1/questions/:id/answers/:id' do
+    let(:user) { create :user }
+    let(:method) { :get }
+    let(:api_path) { "/api/v1/answers/#{answer.id}" }
+    let!(:question) { create(:question, author: user) }
+    let!(:answer) { create(:answer, :with_files, :with_links, :with_comments, question: question, author: user) }
+
+    it_behaves_like 'API Unauthorizable'
+
+    context 'authorized' do
+      let(:answer_response) { json['answer'] }
+      let(:resource) { answer }
+      let(:resource_response) { answer_response }
+
+      before { get api_path, params: { access_token: access_token.token }, headers: headers }
+
+      it_behaves_like 'API Authorizable'
+
+      it_behaves_like 'API returns all public fields' do
+        let(:public_fields) { %w[id body created_at updated_at] }
+      end
+
+      it_behaves_like 'API contains object' do
+        let(:objects) { %w[author question] }
+      end
+
+      it_behaves_like 'API returns list of resource' do
+        let(:resource_contents) { %w[comments files links] }
+      end
+
+      context 'answer links' do
+        it_behaves_like 'API returns all public fields' do
+          let(:resource) { answer.links.first }
+          let(:resource_response) { answer_response['links'].first }
+          let(:public_fields) { %w[id name url created_at updated_at] }
+        end
+      end
+
+      context 'answer comments' do
+        it_behaves_like 'API returns all public fields' do
+          let(:resource) { answer.comments.first }
+          let(:resource_response) { answer_response['comments'].first }
+          let(:public_fields) { %w[id body user_id created_at updated_at] }
+        end
+      end
+
+      it 'contains files url' do
+        expect(answer_response['files'].first['url']).to eq Rails.application.routes.url_helpers.rails_blob_path(
+          answer.files.first, only_path: true
+        )
+      end
+    end
+  end
 end
