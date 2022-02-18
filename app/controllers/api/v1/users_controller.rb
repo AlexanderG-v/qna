978dd05fa20b8ae2360d0957f576a592e5d1a2fc
AlexanderG-v <gorelikov.alexander@gmail.com>
2022-02-18 15:54:49 +0300

    Added a basic controller for the API

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,19 +1,5 @@
-class Api::V1::UsersController < ApplicationController
-  before_action :doorkeeper_authorize!
-
-  authorize_resource
-
+class Api::V1::UsersController < Api::V1::BaseController
   def me
     render json: current_resource_owner
-  end
-
-  private
-
-  def current_resource_owner
-    @current_resource_owner ||= User.find(doorkeeper_token.resource_owner_id) if doorkeeper_token
-  end
-
-  def current_ability
-    @current_ability ||= Ability.new(current_resource_owner)
   end
 end
