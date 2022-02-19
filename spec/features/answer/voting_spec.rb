01d708baa68335f7df784a38a05983db8e36a46f
AlexanderG-v <gorelikov.alexander@gmail.com>
2022-02-19 14:40:22 +0300

    Shared examples spec for 'voting'

--- spec/features/answer/voting_spec.rb
+++ spec/features/answer/voting_spec.rb
@@ -5,74 +5,11 @@ feature 'User can change answer rating by voting', "
   As un authenticated user
   I'd like to be able vote up or down a answer
 " do
-  given(:user) { create(:user) }
-  given(:not_author) { create(:user) }
-  given!(:question) { create(:question, author: user) }
-  given!(:answer) { create(:answer, question: question, author: user) }
-
-  describe 'Unauthenticated user' do
-    scenario "can't to change answer rating by voting" do
-      visit question_path(question)
-
-      expect(page).to_not have_link 'Cancel vote'
-      expect(page).to_not have_link 'UP'
-      expect(page).to_not have_link 'DOWN'
-    end
-  end
-
-  describe 'Authenticated author' do
-    scenario 'tries to change answer rating by voting' do
-      sign_in(user)
-      visit question_path(question)
-
-      expect(page).to_not have_link 'Cancel vote'
-      expect(page).to_not have_link 'UP'
-      expect(page).to_not have_link 'DOWN'
-    end
-  end
-
-  describe 'Authenticated user', js: true do
-    before do
-      sign_in(not_author)
-      visit question_path(question)
-    end
-
-    scenario 'vote up a answer' do
-      within ".answer-id-#{answer.id} .rating" do
-        click_on 'UP'
-
-        expect(page).to have_content '1'
-        expect(page).to have_link 'Cancel vote'
-        expect(page).to_not have_link 'UP'
-        expect(page).to_not have_link 'DOWN'
-      end
-    end
-
-    scenario 'vote down a answer' do
-      within ".answer-id-#{answer.id} .rating" do
-        click_on 'DOWN'
-
-        expect(page).to have_content '-1'
-        expect(page).to have_link 'Cancel vote'
-        expect(page).to_not have_link 'UP'
-        expect(page).to_not have_link 'DOWN'
-      end
-    end
-
-    scenario 'vote cancel a answer' do
-      within ".answer-id-#{answer.id} .rating" do
-        click_on 'UP'
-
-        expect(page).to_not have_link 'UP'
-        expect(page).to_not have_link 'DOWN'
-
-        click_on 'Cancel vote'
-
-        expect(page).to have_content '0'
-        expect(page).to_not have_link 'Cancel vote'
-        expect(page).to have_link 'UP'
-        expect(page).to have_link 'DOWN'
-      end
-    end
+  it_behaves_like 'voting' do
+    given(:user) { create(:user) }
+    given(:not_author) { create(:user) }
+    given!(:question) { create(:question, author: user) }
+    given!(:answer) { create(:answer, question: question, author: user) }
+    given(:resource_class) { ".answer-id-#{answer.id} .rating" }
   end
 end
