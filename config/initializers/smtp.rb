646f35c00bd1b0262b09448720c26662dc05f3cf
AlexanderG-v <gorelikov.alexander@gmail.com>
2022-02-28 01:25:41 +0300

    Setting up action_mailer for production

--- config/initializers/smtp.rb
+++ config/initializers/smtp.rb
@@ -1,6 +1,6 @@
 ActionMailer::Base.smtp_settings = {
-  user_name: Rails.application.credentials[Rails.env.to_sym][:sendgrid][:user_name],
-  password: Rails.application.credentials[Rails.env.to_sym][:sendgrid][:password],
+  user_name: 'apikey',
+  password: Rails.application.credentials[Rails.env.to_sym][:sendgrid][:api_key],
   domain: 'http://194.67.119.77',
   address: 'smtp.sendgrid.net',
   port: 587,
